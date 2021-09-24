51f48a7119bdcd9411abe805ada77b2dda4ab061
yongsang930 <yongsang930@naver.com>
2021-09-24 22:21:38 +0900

    2021-09-24

---
+++ java_src/StandardOfJava/src/Practice6_7/Marine.java
@@ -0,0 +1,23 @@
+package Practice6_7;
+
+
+//static을 붙여야 하는 것을 찾아라 / 공격력 방어력은 공통사항임
+public class Marine {
+	int x = 0, y = 0;          
+	int hp = 60;               
+	int weapon = 6;            //Check
+	int armor = 0;             //Check
+	
+	void weaponUp() {          //Check
+		
+	}
+	
+	void armorUp() {           //Check
+		
+	}
+	
+	void move(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
